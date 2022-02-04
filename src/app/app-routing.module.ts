c8ef457a16f440029a12c5799e79db48685ad59b
chiawen81 <fangchiawen@gmail.com>
2022-02-04 16:16:52 +0800

    完成建環境後備份推

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { Hw1Component } from './hw1/hw1.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Hw2Component } from './hw2/hw2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "home",
+    component: Hw1Component,
+  },
+  {
+    path: "content",
+    component: Hw2Component,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
